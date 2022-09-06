55c01b8607aba2338035726d48bfdb97abdc0230
Nelwhix <nelsonisioma1@gmail.com>
2022-09-06 06:09:14 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+import Navigation from "./components/Navigation.vue";
+
+</script>
+
+<template>
+  <Navigation />
+  <RouterView />
+</template>
