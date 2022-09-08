479112002bba3f6156a549831708e39c49acd573
Nelwhix <nelsonisioma1@gmail.com>
2022-09-08 16:03:45 +0100

    some UI changes

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -10,12 +10,12 @@ const year: string = new Date().getFullYear().toString()
         <p>If you need me for a gig or want to talk about anything Laravel, Tailwind and VueJS. Hit me up!</p>
         <a href="mailto:nelsonisioma1@gmail.com" class="mt-5 border-2 border-body
                 rounded-md px-2 py-4 fill-button fill-image mb-10"><Mail class="inline mr-2 -mt-1" />Send an email</a>
-        <div class="flex justify-between">
+        <div class="flex flex-wrap md:justify-between">
             <div class="flex gap-2 desc text-xl text-white mb-10">
                 <a href="https://github.com/Nelwhix" class="desc"><span>Github</span></a>
                 <a href="https://twitter.com/nelson_isioma" target="_blank" class="desc"><span>Twitter</span></a>
             </div>
-            <p>Copyright © {{ year }} Nelson Isioma</p>
+            <p class>Copyright © {{ year }} Nelson Isioma</p>
         </div>
     </section>
 </template>
