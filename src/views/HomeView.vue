a36021b3e95dea170006168eb842c4c817048e10
Nelson Isioma <nelsonisioma1@gmail.com>
2024-11-26 04:51:54 +0100

    fix: yearly maintenance

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,8 @@ import ProjectCard from "../components/ProjectCard.vue";
 import projects from "../assets/projects.json";
 
 const portfolios = projects;
+const currentDate = new Date();
+const experience = currentDate.getFullYear() - 2019
 </script>
 
 <template>
@@ -18,12 +20,12 @@ const portfolios = projects;
                         from Lagos, Nigeria
                     </span>
                 </p>
-                <p class="mt-5 text-sm md:text-lg">I'm a Fullstack developer with 4+ years experience building products that
-    satisfy client needs. I am diligent and handle projects to the best of my ability
+                <p class="mt-5 text-sm md:text-lg">I'm a Software Engineer with {{experience}}+ years experience building products that
+    satisfy client needs. I am diligent and handle projects to the best of my ability.
                 </p>
                 <a 
                     class="mt-5 border-2 border-body rounded-md p-3 fill-button fill-image mb-10 md:mb-0" 
-                    href="https://docs.google.com/document/d/11oDtGvZLDKa5VRptQ_pXXvbwmoOq6RJT1ueu9gas2Ag/edit?usp=sharing"
+                    href="https://docs.google.com/document/d/1pn_e7lHtNug9uGsp6IaCWP_RmBlMtTzHwTvMquw7yuA/edit?usp=sharing"
                     target="_blank"
                     >
                     Resume
@@ -32,6 +34,7 @@ const portfolios = projects;
         </div>
         <h2 class="mt-10 text-3xl">Projects <span class="opacity-50">( ) {</span></h2>
         <div class="mt-6" id="projects">
+            This is a collection of my side-projects. For my professional work, check out my Resume
             <div class="mb-10 grid grid-flow-row sm:grid-cols-2 sm:gap-4 md:gap-4 md:grid-cols-3">
                 <ProjectCard v-for="portfolio in portfolios" :key="portfolio.id" :portfolio="portfolio" class="transition-transform hover:scale-105 hover:duration-500 ease-in-out" />
             </div>
